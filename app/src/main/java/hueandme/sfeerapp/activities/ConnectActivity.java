b6222245cf7677ecea9d1f460a187a21732b0f81
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2016-12-09 14:00:43 +0100

    proof of concept read temperature

--- app/src/main/java/hueandme/sfeerapp/activities/ConnectActivity.java
+++ app/src/main/java/hueandme/sfeerapp/activities/ConnectActivity.java
@@ -2,8 +2,10 @@ package hueandme.sfeerapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import hueandme.sfeerapp.R;
+import hueandme.sfeerapp.controllers.WeatherController;
 
 public class ConnectActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class ConnectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+
+        TextView tv = (TextView)findViewById(R.id.txtTest);
+        WeatherController ctrl = new WeatherController();
+        tv.setText("" + ctrl.getWeather(this));
     }
 }
