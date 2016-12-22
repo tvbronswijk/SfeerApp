f7384a1cc03e4e4e41ce0af3ea4ce0bbcbc067ed
sander <353331@student.fontys.nl>
2016-12-22 14:28:36 +0100

    timecontroller eerste versie(methodes ingevuld)

--- com.hueandme/src/main/java/com/hueandme/sfeer/TimeController.java
+++ com.hueandme/src/main/java/com/hueandme/sfeer/TimeController.java
@@ -1,12 +1,22 @@
 package com.hueandme.sfeer;
 
+import android.content.Context;
+
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by Tobi on 16-Dec-16.
  */
 
 public class TimeController {
+
+    public TimeController()
+    {
+
+    }
 
     public enum TimeOfDay{
         Morning,
@@ -17,10 +27,35 @@ public class TimeController {
     }
 
     public GregorianCalendar getTime(){
-        throw new UnsupportedOperationException();
+       return new GregorianCalendar(getTimeZone(), getLocale());
     }
 
     public TimeOfDay getTimeOfDay(){
-        throw new UnsupportedOperationException();
+        if(getTime().HOUR_OF_DAY < 6 && getTime().HOUR_OF_DAY >= 0)
+        {
+            return TimeOfDay.Night;
+        }
+        else if(getTime().HOUR_OF_DAY < 12 && getTime().HOUR_OF_DAY >= 6)
+        {
+            return TimeOfDay.Morning;
+        }
+        else if(getTime().HOUR_OF_DAY < 18 && getTime().HOUR_OF_DAY >= 12)
+        {
+            return TimeOfDay.Afternoon;
+        }
+        else if(getTime().HOUR_OF_DAY < 24 && getTime().HOUR_OF_DAY >= 18)
+        {
+            return TimeOfDay.Evening;
+        }
+    }
+
+    private TimeZone getTimeZone()
+    {
+        return TimeZone.getDefault();
+    }
+
+    private Locale getLocale()
+    {
+        return Locale.getDefault();
     }
 }
