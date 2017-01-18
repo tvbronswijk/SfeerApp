d41e98d94e19c17354e7cd40198df47e66425500
Sven Dubbeld <sven.dubbeld1@gmail.com>
2017-01-18 14:12:53 +0100

    Remove configs

--- com.hueandme/src/main/java/com/hueandme/SfeerSettingsListActivity.java
+++ com.hueandme/src/main/java/com/hueandme/SfeerSettingsListActivity.java
@@ -1,8 +1,10 @@
 package com.hueandme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,6 +20,7 @@ import java.util.List;
 public class SfeerSettingsListActivity extends AppCompatActivity {
 
     private SfeerConfigurationController mConfigurationController;
+    private ArrayAdapter<SfeerConfiguration> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +37,58 @@ public class SfeerSettingsListActivity extends AppCompatActivity {
 
         mConfigurationController = new SfeerConfigurationController(this);
 
-        final List<SfeerConfiguration> configurations = new ArrayList<>(mConfigurationController.getAll().values());
-
-        final ArrayAdapter<SfeerConfiguration> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, configurations);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
 
         ListView lstSettings = (ListView) findViewById(R.id.lst_settings);
         lstSettings.setEmptyView(findViewById(android.R.id.empty));
-        lstSettings.setAdapter(adapter);
+        lstSettings.setAdapter(mAdapter);
 
         lstSettings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(SfeerSettingsListActivity.this, SfeerSettingsActivity.class);
-                intent.putExtra("name", adapter.getItem(position).getName());
+                intent.putExtra("name", mAdapter.getItem(position).getName());
                 startActivity(intent);
             }
         });
+
+        lstSettings.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final SfeerConfiguration configuration = mAdapter.getItem(position);
+
+                new AlertDialog.Builder(SfeerSettingsListActivity.this)
+                        .setTitle("Remove " + configuration.getName() + "?")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mConfigurationController.remove(configuration);
+                                reloadList();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        reloadList();
+    }
+
+    private void reloadList() {
+        final List<SfeerConfiguration> configurations = new ArrayList<>(mConfigurationController.getAll().values());
+        mAdapter.clear();
+        mAdapter.addAll(configurations);
     }
 }
